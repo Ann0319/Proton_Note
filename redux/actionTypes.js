27af763ba3569c2df855fa6c2be9875a72102b87
Ann Lin <ann.lin@mile.cloud>
2020-09-24 18:52:10 +0800

    Add API logic.

--- redux/actionTypes.js
+++ redux/actionTypes.js
@@ -1,4 +1,5 @@
 export default {
+    SET_NOTES: 'SET_NOTES',
     CREATE_NOTE: 'CREATE_NOTE',
     EDIT_NOTE: 'EDIT_NOTE',
     DELETE_NOTE: 'DELETE_NOTE'
--- redux/reducers/note.js
+++ redux/reducers/note.js
@@ -1,136 +1,40 @@
 import actionTypes from '../actionTypes'
+import { getNoteList } from '../../lib/api/note'
 
 const initState = {
-    notes: {
-        1: {
-            id: 1,
-            title: 'This is first note.',
-            content: 'This is content of first note',
-            created_at: '2020/09/29 18:00:00',
-            updated_at: '2020/09/29 18:00:00'
-        },
-        2: {
-            id: 2,
-            title: 'This is second note.',
-            content: 'This is content of second note',
-            created_at: '2020/09/29 18:00:00',
-            updated_at: '2020/09/29 18:00:00'
-        },
-        3: {
-            id: 3,
-            title: 'This is third note.',
-            content: 'This is content of third note',
-            created_at: '2020/09/29 18:00:00',
-            updated_at: '2020/09/29 18:00:00'
-        },
-        4: {
-            id: 4,
-            title: 'This is fourth note.',
-            content: 'This is content of fourth note',
-            created_at: '2020/09/29 18:00:00',
-            updated_at: '2020/09/29 18:00:00'
-        },
-        5: {
-            id: 5,
-            title: 'This is fourth note.',
-            content: 'This is content of fourth note',
-            created_at: '2020/09/29 18:00:00',
-            updated_at: '2020/09/29 18:00:00'
-        },
-        6: {
-            id: 6,
-            title: 'This is fourth note.',
-            content: 'This is content of fourth note',
-            created_at: '2020/09/29 18:00:00',
-            updated_at: '2020/09/29 18:00:00'
-        },
-        7: {
-            id: 7,
-            title: 'This is fourth note.',
-            content: 'This is content of fourth note',
-            created_at: '2020/09/29 18:00:00',
-            updated_at: '2020/09/29 18:00:00'
-        },
-        8: {
-            id: 8,
-            title: 'This is fourth note.',
-            content: 'This is content of fourth note',
-            created_at: '2020/09/29 18:00:00',
-            updated_at: '2020/09/29 18:00:00'
-        },
-        9: {
-            id: 9,
-            title: 'This is fourth note.',
-            content: 'This is content of fourth note',
-            created_at: '2020/09/29 18:00:00',
-            updated_at: '2020/09/29 18:00:00'
-        },
-        10: {
-            id: 10,
-            title: 'This is fourth note.',
-            content: 'This is content of fourth note',
-            created_at: '2020/09/29 18:00:00',
-            updated_at: '2020/09/29 18:00:00'
-        },
-        11: {
-            id: 11,
-            title: 'This is fourth note.',
-            content: 'This is content of fourth note',
-            created_at: '2020/09/29 18:00:00',
-            updated_at: '2020/09/29 18:00:00'
-        },
-        12: {
-            id: 12,
-            title: 'This is fourth note.',
-            content: 'This is content of fourth note',
-            created_at: '2020/09/29 18:00:00',
-            updated_at: '2020/09/29 18:00:00'
-        },
-        13: {
-            id: 13,
-            title: 'This is fourth note.',
-            content: 'This is content of fourth note',
-            created_at: '2020/09/29 18:00:00',
-            updated_at: '2020/09/29 18:00:00'
-        },
-        14: {
-            id: 14,
-            title: 'This is fourth note.',
-            content: 'This is content of fourth note',
-            created_at: '2020/09/29 18:00:00',
-            updated_at: '2020/09/29 18:00:00'
-        },
-        15: {
-            id: 15,
-            title: 'This is fourth note.',
-            content: 'This is content of fourth note',
-            created_at: '2020/09/29 18:00:00',
-            updated_at: '2020/09/29 18:00:00'
-        },
-        16: {
-            id: 16,
-            title: 'This is fourth note.',
-            content: 'This is content of fourth note',
-            created_at: '2020/09/29 18:00:00',
-            updated_at: '2020/09/29 20:00:00'
-        }
-    }
+    notes: null
 }
 
 // REDUCERS
 export const noteReducer = (state = initState, action) => {
     const { payload, type } = action
     switch (type) {
-    case actionTypes.CREATE_NOTE:
-        return 
+        case actionTypes.SET_NOTES:
+            return {
+                ...state,
+                notes: payload
+            }
+        case actionTypes.CREATE_NOTE:
+            return
 
-    case actionTypes.EDIT_NOTE:
-        return 
+        case actionTypes.EDIT_NOTE:
+            return
 
-    case actionTypes.DELETE_NOTE:
-        return 
+        case actionTypes.DELETE_NOTE:
+            return
 
-    default:
-        return state
+        default:
+            return state
+    }
+}
+
+export const initNotes = () => async (dispatch, getState) => {
+    const notes = await getNoteList()
+
+    if (notes) {
+        dispatch({
+            type: actionTypes.SET_NOTES,
+            payload: notes
+        })
     }
 }
